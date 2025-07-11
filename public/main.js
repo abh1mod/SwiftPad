53e203b6d6d57b0572115580280aa8cd61286308
abh1mod <absid1120@gmail.com>
2025-07-11 20:30:21 +0530

    canvas done

--- public/main.js
+++ public/main.js
@@ -2,6 +2,9 @@ let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+let backgroundColor = "#dedfec"; // Default background color
+ctx.fillStyle = backgroundColor;
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 let toggleButton = document.getElementById("toggleToolbar"); 
 
 toggleButton.addEventListener("click", function() {
@@ -16,6 +19,13 @@ toggleButton.addEventListener("click", function() {
 let draw_color = "black";
 let draw_width = "5";
 let is_drawing = false;
+let restore_array = [];
+let index = -1;
+
+
+function change_color(element) {
+    draw_color = element.style.backgroundColor;
+}
 
 canvas.addEventListener("touchstart", start, false);
 canvas.addEventListener("touchmove", draw, false);
@@ -31,9 +41,12 @@ function start(e) {
     ctx.moveTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
     e.preventDefault();
 }   
+
 function draw(e) {
     if (!is_drawing) return;
-    ctx.lineTo(e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    let x = e.clientX - canvas.offsetLeft;
+    let y = e.clientY - canvas.offsetTop;
+    ctx.lineTo(x, y);
     ctx.strokeStyle = draw_color;
     ctx.lineWidth = draw_width;
     ctx.lineCap = "round";
@@ -41,6 +54,7 @@ function draw(e) {
     ctx.stroke();
     e.preventDefault();
 }
+
 function stop(e) {
     if(is_drawing) {
         ctx.stroke();
@@ -48,4 +62,27 @@ function stop(e) {
         is_drawing = false;
     }
     e.preventDefault();
+    if(e.type !== "mouseout") {
+        restore_array.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
+        index += 1;
+        console.log(restore_array);
+    }
+    
 }   
+
+function clearCanvas() {
+    ctx.fillStyle = backgroundColor; 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    restore_array = [];
+    index = -1;
+}
+
+function undo_last(){
+    if(index <= 0) {
+        clearCanvas();
+        return;
+    }
+    index -= 1;
+    ctx.putImageData(restore_array[index], 0, 0);
+}
